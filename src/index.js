d3393f0d19d7877cc10e522177208e16124ca587
Tim Supan <timsupann@gmail.com>
2023-11-22 19:52:12 -0800

    Create DOM div elements in js for header section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("wassup");
+loadHeader();
+function loadHeader(){
+    const contentDiv = document.querySelector('content');
+
+    const header = document.createElement('div');
+    header.classList.add('header');
+    contentDiv.appendChild(header);
+
+    const header_content = document.createElement('div');
+    header_content.classList.add('header_content');
+    const topRtitle = document.createElement('p');
+    topRtitle.innerText = "Tim's Diner";
+    header_content.appendChild(topRtitle);
+
+    const header_menu = document.createElement('div');
+    header_content.classList.add('header_menu');    
+    const menus = document.createElement('p');
+    menus.innerText = "Menus";
+    header_menu.appendChild(menus);
+    const locations = document.createElement('p');
+    locations.innerText = "Locations";
+    header_menu.appendChild(locations);
+    const giftcards = document.createElement('p');
+    giftcards.innerText = "Gift Cards";
+    header_menu.appendChild(giftcards);
+    const careers = document.createElement('p');
+    careers.innerText = "Careers";
+    header_menu.appendChild(careers);
+}
